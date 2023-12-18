89824da80828f857e94cd73bc129acdb89d89175
marcsilva <marciliodevjava@gmail.com>
2023-12-18 20:20:34 -0300

    create

---
+++ main.py
@@ -0,0 +1,22 @@
+idade1 = 39
+idade2 = 30
+idade3 = 27
+idade4 = 18
+
+print(idade1)
+print(idade2)
+print(idade3)
+print(idade4)
+print()
+
+idades = [idade1, idade2, idade3, idade4]
+idades.append(15)
+
+for idade in idades:
+    print(idade)
+
+print()
+
+for ida in range(1,11):
+    print(ida)
+
