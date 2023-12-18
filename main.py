2e62ebd839f5ebaecd96f9321117236eb9072d3d
marcsilva <marciliodevjava@gmail.com>
2023-12-18 20:40:02 -0300

    Trabalhando com listas

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ print(idade4)
 print()
 
 idades = [idade1, idade2, idade3, idade4]
+idades_num = [idade1, idade2, idade3, idade4]
 idades.append(15)
 
 for idade in idades:
@@ -33,6 +34,7 @@ print(idades)
 print(idades[5][1])
 
 numeros = [3, 5, 9, 12]
+list = [39, 78, 48, 58]
 lista = [39, 78, 48, 58, numeros]
 
 idades.append(lista)
@@ -41,3 +43,20 @@ print(idades)
 print(idades[6][0])
 print(idades[6][4])
 print(idades[6][4][3])
+
+num = [9, 1]
+
+num.extend(idades_num)
+num.extend(lista_numeros)
+num.extend(list)
+
+print(num)
+print()
+
+lista_idade = [22, 36, 25, 14]
+print(lista_idade)
+print()
+
+lista_idade_ano_que_vem = [(ida + 1) for ida in lista_idade]
+print(lista_idade_ano_que_vem)
+
